09d338ad62452ccc546a40657437ab96a56ca3a5
azegc <azeneth.garciacst@uanl.edu.mx>
2021-05-19 21:52:30 -0500

    def

---
+++ Cursos.py
@@ -0,0 +1,5 @@
+class Cursos:
+    def __init__(self, id_curso=5, descripcion="Desarrollo", id_empleado=5):
+        self.__id_curso = id_curso
+        self.__descripcion = descripcion
+        self.__id_empleado = id_empleado
