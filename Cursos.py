bb865a92a323c2863ac7d5b0899fd628f61b0d55
azegc <azeneth.garciacst@uanl.edu.mx>
2021-05-19 22:09:04 -0500

    Cursos txt

--- Cursos.py
+++ Cursos.py
@@ -7,3 +7,11 @@ class Cursos:
         self.__id_curso = id_curso
         self.__descripcion = descripcion
         self.__id_empleado = id_empleado
+        archivo = open("cursos.txt", 'a')
+        archivo.write(id_curso)
+        archivo.write("|")
+        archivo.write(descripcion)
+        archivo.write("|")
+        archivo.write(id_empleado)
+        archivo.write('\n')
+        archivo.close()
